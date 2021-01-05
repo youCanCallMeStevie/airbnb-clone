cbdd4b2692f86ba5976c125f15d5e14b5497821e
youCanCallMeStevie <72197493+youCanCallMeStevie@users.noreply.github.com>
2021-01-05 19:40:56 +0000

    expanded search bar form

--- src/Components/ExpandedSearchBar/ExpandedSearchBar.jsx
+++ src/Components/ExpandedSearchBar/ExpandedSearchBar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useRef } from "react";
 import { Col, Navbar, Row } from "react-bootstrap";
 import "../ExpandedSearchBar/ExpandedSearchBar.css";
 import PinkLogo from "../../Assets/AirBnb-Logos/airbnb_pink_logo_no_text.png";
@@ -8,16 +8,31 @@ import MenuIcon from "@material-ui/icons/Menu";
 import PersonOutlineIcon from "@material-ui/icons/PersonOutline";
 import UserDropDownMenu from "../UserDropDownMenu/UserDropDownMenu";
 import { Divider } from "@material-ui/core";
+import useOutsideClick from "../../Utilites/Hooks/ClickOutside";
 
-export default function ExpandedSearchBar(
-  {
-      handleChange,
-      handleSubmit,
-      clicked,
-      handleClick,
-      toggleModal,
-  }
-) {
+
+export default function ExpandedSearchBar({
+  clicked,
+  handleClick,
+  toggleModal,
+})
+
+{
+
+    const [searchActive, setSearchActive] = useState(false);
+    const [search, setSearch] = useState("");
+
+    const ref = useRef();
+
+    useOutsideClick(ref, () => {
+        if (searchActive) setSearchActive(true);
+      });
+
+    const submit = e => {
+      e.preventDefault();
+      alert(`We're having a look for ${search}`);
+      setSearch("");
+    };
   return (
     <div className="expanded-navbar-container">
       <Navbar>
@@ -30,82 +45,100 @@ export default function ExpandedSearchBar(
             alt="AirBnB logo"
           />
         </Navbar.Brand>
-            <div
-              className="expanded-navbar-item-wrap"
-              style={{ marginRight: "5px", marginTop: "3px" }}
-            >
-              <div>
-                <p>Switch to hosting</p>
-              </div>
-              <div
-                className="choose-language"
-                  onClick={() => toggleModal()}
-              >
-                <LanguageIcon />{" "}
-              </div>
-              <div
-                onClick={handleClick}
-              >
-                {clicked ? <UserDropDownMenu /> : null}
-                <div className="search-expanded-user-hamburger-wrapper">
-                  <MenuIcon />
-                  <PersonOutlineIcon />
-                </div>
-              </div>
+        <div
+          className="expanded-navbar-item-wrap"
+          style={{ marginRight: "5px", marginTop: "3px" }}
+        >
+          <div>
+            <p>Switch to hosting</p>
+          </div>
+          <div className="choose-language" onClick={() => toggleModal()}>
+            <LanguageIcon />{" "}
+          </div>
+          <div onClick={handleClick}>
+            {clicked ? <UserDropDownMenu /> : null}
+            <div className="search-expanded-user-hamburger-wrapper">
+              <MenuIcon />
+              <PersonOutlineIcon />
             </div>
-          </Navbar>
-          <Col>
-
-          <Row>
-            <div className="expanded-navbar-item-wrap">
-              <div className="search-menu-title">
-                <p>Places to stay</p>
-              </div>
-              <div className="search-menu-title">
-                <p>Experiences</p>
-              </div>
-              <div className="search-menu-title">
-                <p>Online experiences</p>
-              </div>
+          </div>
+        </div>
+      </Navbar>
+      <Col>
+        <Row>
+          <div className="expanded-navbar-item-wrap">
+            <div className="search-menu-title">
+              <p>Places to stay</p>
             </div>
+            <div className="search-menu-title">
+              <p>Experiences</p>
+            </div>
+            <div className="search-menu-title">
+              <p>Online experiences</p>
+            </div>
+          </div>
+        </Row>
+        <Row className="mt-2 search-filter-box">
+          <Row className="search-filter-items">
+              <form>
+            <Col md="auto" className="search-filter-item">
+              <p>Location</p>
+                <input
+                  value={search}
+                  type="text"
+                  placeholder="Where are you going?"
+                  onChange={e => setSearch(e.target.value)}
+                />
+            </Col>
+            <Divider orientation="vertical" flexItem />
+            <Col md="auto" className="search-filter-item">
+              <p>Check in </p>
+
+              <input
+                  value={search}
+                  type="date"
+                  placeholder="Add dates"
+                  onChange={e => setSearch(e.target.value)}
+                />
+            </Col>
+            <Divider orientation="vertical" flexItem />
+
+            <Col md="auto" className="search-filter-item">
+              <p>Check out </p>
+              <input
+                  value={search}
+                  type="date"
+                  placeholder="Add dates"
+                  onChange={e => setSearch(e.target.value)}
+                />
+            </Col>
+            <Divider orientation="vertical" flexItem />
+
+            <Col md="auto" className="search-filter-item">
+              <p>Guests </p>
+              <input
+                  value={search}
+                  type="number"
+                  placeholder="Add guests"
+                  onChange={e => setSearch(e.target.value)}
+                />
+            </Col>
+            <Col md="auto">
+              <div className="expanded-search-background">
+                <span>
+                  <SearchIcon />
+                </span>
+                <span>
+                  <p style={{ paddingLeft: "2px", marginBottom: "10px" }}>
+                    Search
+                  </p>
+                </span>
+              </div>
+            </Col>
+            </form>
           </Row>
-          <Row className="mt-2 search-filter-box">
-            
-              <Row className="search-filter-items">
-                <Col md="auto" className="search-filter-item">
-                <p>Location</p>
-
-                <p className="text-muted" style={{fontWeight:"400"}}>Where are you going?</p>
-
-                </Col>
-                <Divider orientation="vertical" flexItem />
-                <Col md="auto" className="search-filter-item">
-                  <p>Check in </p>
-                  <p className="text-muted" style={{fontWeight:"400"}}>Add dates</p>
-                </Col>
-                <Divider orientation="vertical" flexItem />
-
-                <Col md="auto" className="search-filter-item">
-                  <p>Check out </p>
-                  <p className="text-muted" style={{fontWeight:"400"}}>Add dates</p>
-
-                </Col>
-                <Divider orientation="vertical" flexItem />
-
-                <Col md="auto" className="search-filter-item">
-                  <p>Guests </p>
-                  <p className="text-muted" style={{fontWeight:"400"}}>Add guests</p>
-
-                </Col>
-                <Col md="auto">
-                  <div className="expanded-search-background">
-                    <span><SearchIcon /></span><span><p style={{paddingLeft:"2px", marginBottom:"10px"}}>Search</p></span>
-                  </div>
-                </Col>
-              </Row>
-           
-          </Row>
-        </Col>
+        </Row>
+      </Col>
     </div>
   );
 }
